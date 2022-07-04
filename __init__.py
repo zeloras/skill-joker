1b5a53b598bb0febb1526408a6897205a38c4bc2
krasnoglazik <zeloras@gmail.com>
2022-07-04 16:42:47 +0300

    added selfie mode

--- __init__.py
+++ __init__.py
@@ -12,26 +12,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from os import path, mkdir
+
+from os import path, makedirs
 from os.path import expanduser
 from datetime import date
 
 import pyautogui
 
 from adapt.intent import IntentBuilder
+from mycroft.skills import skill_api_method
 from mycroft.skills.core import MycroftSkill, intent_handler
-import pyclip
 
 
-class JokerSkill(MycroftSkill):
+class HelpersMixin:
+    @staticmethod
+    def create_if_not_exists(directory):
+        """
+        Create dir if not exist
+        :param directory:
+        :return:
+        """
+        if not path.isdir(directory):
+            makedirs(directory)
+
+
+class ErrorsMixin(MycroftSkill):
+    def handle_error(self):
+        self.speak("I am shit myself")
+
+
+class CameraShootMixin(MycroftSkill):
+    @skill_api_method
+    def take_single_photo(self):
+        """Take a single photo using the attached camera."""
+        self.handle_camera_activity()
+
+    def handle_camera_activity(self):
+        """Perform camera action.
+
+        Arguments:
+            activity (str): the type of action to take, one of:
+                "generic" - open the camera app
+        """
+        self.gui["save_path"] = self.save_folder
+        self.gui["singleshot_mode"] = True
+        self.gui.show_page("Camera.qml", override_idle=60)
+
+
+class JokerSkill(MycroftSkill, HelpersMixin, CameraShootMixin):
+
     def __init__(self):
         self.screenshot_dir = expanduser("~/Pictures")
-        if not path.exists(self.screenshot_dir):
-            mkdir(self.screenshot_dir)
+        self.videos_dir = expanduser("~/Videos")
+        self.create_if_not_exists(self.screenshot_dir)
+        self.create_if_not_exists(self.videos_dir)
         super(JokerSkill, self).__init__(name="JokerSkill")
-
-    def bad_boy(self):
-        self.speak("I am shit myself, check my shit")
 
     @intent_handler(IntentBuilder("ScreenshotIntent").require("Screenshot"))
     def handle_screenshot(self):
@@ -44,14 +79,15 @@ class JokerSkill(MycroftSkill):
             my_screenshot.save(filepath)
             # TODO: Better don't kill after CTRL+v EVERYTHING
             # pyclip.copy(open(filepath, 'rb').read())
-            self.speak("Saved")
+            self.take_single_photo()  # Take a smile!
+            self.speak("Shoot!")
         except NotImplementedError as e:
             print(e)
-            self.bad_boy()
+            self.handle_error()
 
     def stop(self):
+        # self.handle_camera_completed()
         pass
-
 
 def create_skill():
     return JokerSkill()
